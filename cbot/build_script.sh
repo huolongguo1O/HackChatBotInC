89a0bc60d6e115753c6d6e0835390f5f7c13de30
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2024-04-14 20:45:57 +0800

    Add brainfucked changes

--- cbot/build_script.sh
+++ cbot/build_script.sh
@@ -1,7 +1,7 @@
 # git clone https://github.com/curl/curl.git
 # You can down load by submodule
 cd curl
-cmake . -DENABLE_WEBSOCKETS=ON -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DHTTP_ONLY=ON
+cmake . -DENABLE_WEBSOCKETS=OFF -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DHTTP_ONLY=ON
 make -j4
 make install
 cd ..
